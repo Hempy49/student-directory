74f4b5d1ac20eb9169e0e91e3e1bdad950399685
Simon Hempenstall <simonjhemp@hotmail.com>
2017-05-22 15:25:59 +0100

    Fix singular/ plural form

--- directory.rb
+++ directory.rb
@@ -1,11 +1,11 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the name of the student"
   puts "To finish, just hit return twice"
   students = []
   name = gets.chomp
   while !name.empty? do
     students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} #{students.count == 1 ? "student" : "students"}"
     name = gets.chomp
   end
   students
@@ -23,7 +23,8 @@ def print(students)
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  count = students.count
+  puts "Overall, we have #{count} great #{count == 1 ? "student" : "students"}"
 end
 
 students = input_students
