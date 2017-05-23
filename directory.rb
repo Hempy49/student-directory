b78e6a29e8765ca25d210dd02907aa423193110c
Simon Hempenstall <simonjhemp@hotmail.com>
2017-05-23 12:32:00 +0100

    Add height and DOB to program

--- directory.rb
+++ directory.rb
@@ -1,11 +1,17 @@
 def input_students
   puts "Please enter the name of the student"
-  puts "To finish, just hit return twice"
+  puts "To finish, just hit return"
   students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    puts "Now enter their height:"
+    height = gets.chomp
+    puts "Enter their date of birth (dd/mm/yy):"
+    d_o_b = gets.chomp
+    students << {name: name, height: height, d_o_b: d_o_b, cohort: :november}
     puts "Now we have #{students.count} #{students.count == 1 ? "student" : "students"}"
+    puts "Please enter next students name"
+    puts "To finish, just hit return"
     name = gets.chomp
   end
   students
@@ -18,11 +24,7 @@ end
 
 def print(students)
     students.each_with_index do |student, index|
-        if student[:name].length < 12
-            puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-        else
-          puts ""
-        end
+            puts "#{index + 1}. #{student[:name]}, #{student[:height]}, #{student[:d_o_b]} (#{student[:cohort]} cohort)"
     end
 end
 
