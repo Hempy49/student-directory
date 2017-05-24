ae24bc7b9a0a093a1561d4fd4abe4f09f26707ce
Simon Hempenstall <simonjhemp@hotmail.com>
2017-05-24 11:32:48 +0100

    Change input_students method to ask for name and cohort

--- directory.rb
+++ directory.rb
@@ -2,33 +2,40 @@ def input_students
   puts "Please enter the name of the student"
   puts "To finish, just hit return"
   students = []
-  name = gets.chomp
+  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  name = gets.chomp 
+
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} #{students.count == 1 ? "student" : "students"}"
-    puts "Please enter next students name"
-    puts "To finish, just hit return"
-    name = gets.chomp
+    puts "Now enter their cohort:"
+    cohort = gets.chomp
+    if !months.include? cohort
+      puts "Cohort not found. Options are #{months}"
+    else
+      students << {name: name, cohort: cohort}
+      puts "Now we have #{students.count} #{students.count == 1 ? "student" : "students"}"
+      puts "Please enter next students name"
+      puts "To finish, just hit return"
+      name = gets.chomp
+    end
   end
   students
 end
 
+
 def print_header
-  puts ("The students of Villains Academy").center(50)
-  puts ("-------------").center(50)
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
 def print(students)
-  count = 0
-  while count < students.count
-    puts ("#{students[count][:name]} (#{students[count][:cohort]} cohort)").center(50)
-    count = count + 1
-  end
+    students.each_with_index do |student, index|
+            puts "#{index + 1}. #{student[:name]}, (#{student[:cohort]} cohort)"
+    end
 end
 
 def print_footer(students)
   count = students.count
-  puts ("Overall, we have #{count} great #{count == 1 ? "student" : "students"}").center(50)
+  puts "Overall, we have #{count} great #{count == 1 ? "student" : "students"}"
 end
 
 students = input_students
