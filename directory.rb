b081363f4f55c1b49d56980e9969ed12ea9bd557
Simon Hempenstall <simonjhemp@hotmail.com>
2017-05-24 20:17:35 +0100

    Refactor code

--- directory.rb
+++ directory.rb
@@ -1,43 +1,46 @@
-def interactive_menu
-  students = []
-  loop do
-    #1.print menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    #2. read the input and save it into a variable
-    selection = gets.chomp
-    #3. do what the user has asked
-    case selection
-    when "1"
-      #input students
-      students = input_students
-    when "2"
-      #show students
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-    end
-    #4. repeat from step1
+@students = []
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer()
+end
+
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
   end
 end
 
 def input_students
   puts "Please enter the name of the student"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} #{students.count == 1 ? "student" : "students"}"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} #{@students.count == 1 ? "student" : "students"}"
     name = gets.chomp
   end
-  students
 end
 
 def print_header
@@ -45,14 +48,14 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
+def print_student_list()
+  @students.each_with_index do |student, index|
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  count = students.count
+def print_footer
+  count = @students.count
   puts "Overall, we have #{count} great #{count == 1 ? "student" : "students"}"
 end
 
